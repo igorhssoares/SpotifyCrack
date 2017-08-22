e13e596867d6c63a94f084631e42e971bb4d3b67
JustAbP <31173835+JustAbP@users.noreply.github.com>
2017-08-22 18:04:05 -0300

    Update SpotifyCrack.py

--- SpotifyCrack.py
+++ SpotifyCrack.py
@@ -32,41 +32,41 @@ if sistema == 'Windows':
             diretorio = os.chdir('C:\Windows\System32\drivers\etc')
             with open('hosts','a') as crack:
                 crack.write("""
-    0.0.0.0 adclick.g.doublecklick.net
-    0.0.0.0 googleads.g.doubleclick.net
-    0.0.0.0 http://www.googleadservices.com
-    0.0.0.0 pubads.g.doubleclick.net
-    0.0.0.0 securepubads.g.doubleclick.net
-    0.0.0.0 pagead2.googlesyndication.com
-    0.0.0.0 spclient.wg.spotify.com
-    0.0.0.0 audio2.spotify.com
+0.0.0.0 adclick.g.doublecklick.net
+0.0.0.0 googleads.g.doubleclick.net
+0.0.0.0 http://www.googleadservices.com
+0.0.0.0 pubads.g.doubleclick.net
+0.0.0.0 securepubads.g.doubleclick.net
+0.0.0.0 pagead2.googlesyndication.com
+0.0.0.0 spclient.wg.spotify.com
+0.0.0.0 audio2.spotify.com
     """)
                 print('[+]Crackeado com sucesso !!! ')
         elif escolha == 2:
             diretorio = os.chdir('C:\Windows\System32\drivers\etc')
             with open('hosts','w') as remove:
                 remove.write("""
-    # Copyright (c) 1993-2009 Microsoft Corp.
-    #
-    # This is a sample HOSTS file used by Microsoft TCP/IP for Windows.
-    #
-    # This file contains the mappings of IP addresses to host names. Each
-    # entry should be kept on an individual line. The IP address should
-    # be placed in the first column followed by the corresponding host name.
-    # The IP address and the host name should be separated by at least one
-    # space.
-    #
-    # Additionally, comments (such as these) may be inserted on individual
-    # lines or following the machine name denoted by a '#' symbol.
-    #
-    # For example:
-    #
-    #      102.54.94.97     rhino.acme.com          # source server
-    #       38.25.63.10     x.acme.com              # x client host
+# Copyright (c) 1993-2009 Microsoft Corp.
+#
+# This is a sample HOSTS file used by Microsoft TCP/IP for Windows.
+#
+# This file contains the mappings of IP addresses to host names. Each
+# entry should be kept on an individual line. The IP address should
+# be placed in the first column followed by the corresponding host name.
+# The IP address and the host name should be separated by at least one
+# space.
+#
+# Additionally, comments (such as these) may be inserted on individual
+# lines or following the machine name denoted by a '#' symbol.
+#
+# For example:
+#
+#      102.54.94.97     rhino.acme.com          # source server
+#       38.25.63.10     x.acme.com              # x client host
 
-    # localhost name resolution is handled within DNS itself.
-    #	127.0.0.1       localhost
-    #	::1             localhost
+# localhost name resolution is handled within DNS itself.
+#	127.0.0.1       localhost
+#	::1             localhost
     """)
                 print('[+]Remoçao de ADs concluida.Voltando ao original!!')
                 exit()
